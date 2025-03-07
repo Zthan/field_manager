1bea0c8d7542c2b157b399e5277c0b4b458b5bc8
Zthan <38104256+Zthan@users.noreply.github.com>
2025-03-07 06:22:34 +0000

    text updates

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,14 @@
 import streamlit as st
 from pybaseball import spraychart, playerid_lookup, statcast_batter, statcast, chadwick_register
 import pandas as pd
-#import matplotlib.pyplot as plt
-#from pybaseball import plotting
-#from PIL import Image, ImageDraw
-#import io
-import requests
-from io import StringIO
+
+
 
 st.title("Field Manager Spraychart Generator")
 st.write(
-    "This is a simple app. Enter a MLB player's first and last name only, no Jr's or II or anything else. Then select the stadium you want to see their spraychart for."
+    "Choose an MLB player and then select on which stadium you want to see their hits overlayed."
     "The 'Toggle Only Hits at this Stadium' toggle will turn all hits for a player on and off."
+    "Year Toggles will turn on or off the hit data from that year."
 )
 
 options_dict = {
@@ -54,8 +51,6 @@ data = chadwick_register()
 data = data.loc[data['mlb_played_last'].isin([2024, 2023, 2022])]
 full_names = data.apply(lambda row: f"{row['name_first']} {row['name_last']}", axis=1).tolist()
 
-#name_first = entered_name.split(' ')[0]
-#name_last = entered_name.split(' ')[1]
 
 entered_name = st.selectbox("Pick an MLB Player.", full_names)
 name_first = entered_name.split(' ')[0]
@@ -65,6 +60,9 @@ name_last = entered_name.split(' ')[1]
 team_stadium = st.selectbox("Choose a stadium:", options_list)
 team_stadium_display = options_dict_2.get(team_stadium, None)
 away_off = st.checkbox('Toggle Only Hits at this Stadium')
+year_2024 = st.checkbox('2024')
+year_2023 = st.checkbox('2023')
+year_2022 = st.checkbox('2022')
 
 if name_first and name_last:
     lookup_number = playerid_lookup(name_last, name_first, fuzzy=True)
@@ -86,10 +84,20 @@ if name_first and name_last:
                              'rangers': 'TEX', 'rays': 'TB', 'red_sox': 'BOS', 'reds': 'CIN', 'rockies': 'COL',
                              'royals': 'KC', 'tigers': 'DET', 'twins': 'MIN', 'white_sox': 'CWS', 'yankees': 'NYY'}
 
+        
         home_team = team_stadium_dict.get(team_stadium_display, None)
         chart_title = f"{hitter_name_first} {hitter_name_last} @ {team_stadium_display} Stadium"
 
-        pitch_data = statcast_batter('2024-03-28', '2024-09-30', hitter)
+        # working on year toggle, needs more work before committing
+        #def cum_pitch_data():
+
+        #if year_2023:
+        #    pitch_data_23 = statcast_batter('2023-03-30', '2023-10-01', hitter)
+        #    pitch_data_23 = pitch_data_23.loc[pitch_data_23['batter'] == hitter]
+        #    pitch_data_23 = pitch_data_23.loc[pitch_data_23['events'].isin(['single', 'double', 'triple', 'home_run'])]
+        #    pitch_data_23.sort_values('events')
+            
+        pitch_data = statcast_batter('2024-03-20', '2024-09-30', hitter)
         pitch_data = pitch_data.loc[pitch_data['batter'] == hitter]
         pitch_data = pitch_data.loc[pitch_data['events'].isin(['single', 'double', 'triple', 'home_run'])]
         pitch_data.sort_values('events')
