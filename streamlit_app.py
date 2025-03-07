5d621b762b4219717863cb3fbb1e6259c1d38370
Zthan <38104256+Zthan@users.noreply.github.com>
2025-03-07 06:00:07 +0000

    new name input

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,6 +40,7 @@ options_list = ['Generic', 'Angels', 'Astros', 'Athletics', 'Blue Jays', 'Braves
                 ]
 # get the register data
 data = chadwick_register()
+'''
 def load_original_data():
     url = 'https://raw.githubusercontent.com/Zthan/field_manager/refs/heads/main/chadwick.csv'
     response = requests.get(url)
@@ -47,8 +48,9 @@ def load_original_data():
         return pd.read_csv(StringIO(response.text))
     else:
         st.error("Failed to load data from GitHub.")
-        return None
-#data = load_original_data()
+        return None'
+'''
+
 data = data.loc[data['mlb_played_last'].isin([2024, 2023, 2022])]
 full_names = data.apply(lambda row: f"{row['name_first']} {row['name_last']}", axis=1).tolist()
 
@@ -56,10 +58,10 @@ full_names = data.apply(lambda row: f"{row['name_first']} {row['name_last']}", a
 #name_last = entered_name.split(' ')[1]
 
 entered_name = st.selectbox("Pick an MLB Player.", full_names)
-#name_first = entered_name.split(' ')[0]
-#name_last = entered_name.split(' ')[1]
-name_first = st.text_input("Enter Player First Name:")
-name_last = st.text_input("Enter Player Last Name:")
+name_first = entered_name.split(' ')[0]
+name_last = entered_name.split(' ')[1]
+#name_first = st.text_input("Enter Player First Name:")
+#name_last = st.text_input("Enter Player Last Name:")
 team_stadium = st.selectbox("Choose a stadium:", options_list)
 team_stadium_display = options_dict_2.get(team_stadium, None)
 away_off = st.checkbox('Toggle Only Hits at this Stadium')
